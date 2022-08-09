6c3a7be2a01e8aa05f74c4446bb73809f5c4a3ec
habebaRabie <habeba.rabiee@outlook.com>
2022-08-09 09:13:37 +0200

    add swagger documentation

--- src/main/java/com/ntg/organization/organization/OrganizationApplication.java
+++ src/main/java/com/ntg/organization/organization/OrganizationApplication.java
@@ -2,11 +2,10 @@ package com.ntg.organization.organization;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.web.client.RestTemplate;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+//@EnableSwagger2
 public class OrganizationApplication {
 
 	public static void main(String[] args) {
@@ -14,3 +13,5 @@ public class OrganizationApplication {
 	}
 
 }
+
+//http://localhost:8090/swagger-ui.html
